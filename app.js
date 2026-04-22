4ea754fab24795a1e3bf525ea5ce2aa66ed962e2
muhammed-ahmed877 <ahmeddehware@gmail.com>
2026-04-21 21:12:28 -0700

    pracitc the firts qustion

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("welcome to the project of operators");
+
+
+
+// let num1=  prompt("kindly enter the your first number");
+// let num2= prompt("kindly enter the 2nd number ");
+// let sum= Number(num1) + Number(num2);
+// alert ("this is the number you added "+ sum)
+
+
+
+
+
+
+// let num1= prompt("kindly enter the number");
+// let num2=prompt ("kindly enter the 2nd num")
+// let sum = (num1)-(num2);
+// console.log("here is the number that yoe want to subtract"  +  sum);
+
+
+
