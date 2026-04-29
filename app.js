86fe409c4335bf3778f91b94191527c5f599f130
muhammed-ahmed877 <ahmeddehware@gmail.com>
2026-04-28 20:01:08 -0700

    practice

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ console.log("welcome to the project of operators");
 
 
 
+// ✴️ Arithmetic Operator Tasks
+
+
+
+// 1. Ask the user to enter two numbers. Show the result of adding them using alert().
+
 // let num1=  prompt("kindly enter the your first number");
 // let num2= prompt("kindly enter the 2nd number ");
 // let sum= Number(num1) + Number(num2);
@@ -12,10 +18,62 @@ console.log("welcome to the project of operators");
 
 
 
+// 2. Ask for two numbers. Subtract the second number from the first and log the result to the console.
 // let num1= prompt("kindly enter the number");
 // let num2=prompt ("kindly enter the 2nd num")
 // let sum = (num1)-(num2);
-// console.log("here is the number that yoe want to subtract"  +  sum);
+// console.log("here is the number that you want to subtract"  +  sum);
 
 
 
+
+// 3. Ask for two numbers. Multiply them and display the answer in an alert.
+// let num1=prompt("kindly add your first number that you want to multiply");
+// let num2= prompt("kindly add your second number that you want to multiply")
+// let sum =(num1)*(num2);
+// alert("this is the number you multiplied " + sum);
+
+// 4. Ask the user for two numbers. Divide the first by the second and log the result.
+// let num1 = prompt("kindly enter the first number that you want to multiply")
+// let num2 = prompt("kindly enter the second number that you want to multiply")
+// let num3 = (num1) / (num2);
+// console.log("here is the number that you want to divide" + num3);
+
+// 5. Ask the user for two numbers and show the remainder when the first is divided by the second (use %).
+// let num1 = prompt ("kindly enter the first number that you want to divide")
+// let num2 = prompt ("kindly enter the second number that you want to divide")
+// let num3 = (num1) % (num2);
+// alert("this is the number you get as a remainder " + num3);
+
+
+
+
+
+
+
+
+
+
+// ✴️ Assignment Operator Tasks
+// 8. Create a variable with the value 6. Multiply it by 3 using *= and display the result.
+// 9. Set a variable to 50. Divide it by 2 using /= and log the final value.
+// 10. Create a variable, assign it a value from prompt(), then increase it by 10 and show the
+// result.
+
+
+
+
+// 6. Create a variable with the value 10. Add 5 to it using += and log the new value.
+//  var num1=(x=20);
+//  var num2 = (x = 50);
+//  console.log((num1) += (num2)  );
+
+
+
+
+
+
+// 7. Create a variable with the value 20. Subtract 4 from it using -= and alert the result.
+// var num1=(x=20);
+// var num2=(x=4);
+// alert((num1) -= (num2) );    
